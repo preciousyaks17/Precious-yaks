3f8028692c181f7d320156910795e5ad7d866283
preciousyaks17 <johndoe@example.com>
2024-07-05 21:06:01 +0100

    updated navbar component

--- Components/navbar.jsx
+++ Components/navbar.jsx
@@ -8,40 +8,6 @@ const Navbar = () => {
           Pre<span className="text-blue-500">Yaks.</span>
         </li>
       </ul>
-      {/* <ul className="flex flex-end">
-        <li className="mr-5">
-          <Link
-            href="/"
-            className="primary font-medium text-sm hover:text-blue-500"
-          >
-            Home
-          </Link>
-        </li>
-        <li className="mr-5">
-          <Link
-            href="#about"
-            className="primary font-medium text-sm hover:text-blue-500"
-          >
-            About
-          </Link>
-        </li>
-        <li className="mr-5">
-          <Link
-            href="#contact"
-            className="primary font-medium text-sm hover:text-blue-500"
-          >
-            Contact
-          </Link>
-        </li>
-        <li className="mr-5">
-          <Link
-            href="#projects"
-            className="primary font-medium text-sm hover:text-blue-500"
-          >
-            Projects
-          </Link>
-        </li>
-      </ul> */}
     </nav>
   );
 };
